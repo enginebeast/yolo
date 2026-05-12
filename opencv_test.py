1fda8d0434204c670c10f033babb86723ed80411
Lee JungHwan <well0914@naver.com>
2026-05-12 23:54:31 +0900

    extract coordinate

--- opencv_test.py
+++ opencv_test.py
@@ -7,13 +7,22 @@ import cv2
 # Reading boundingbox
 box = pd.read_csv("./cardataset/train_solution_bounding_boxes (1).csv")
 box.info()
-print(box)
+box
 
 # Writing boundingbox
 sample1 = cv2.imread("./cardataset/training_images/vid_4_1000.jpg")
-print(sample1)
+sample1
 
-# Define color sequence
+# Define color channel sequence
 sample = cv2.cvtColor(sample1, cv2.COLOR_BGR2RGB)
-print(sample)
+sample
 
+# Extract xy coordination
+point = box.iloc[0]
+point, type(point)
+
+# Extract start coordinate and end coordinate
+pt1 = (int(point.xmin), int(point.ymin))
+pt2 = (int(point.xmax), int(point.ymax))
+
+pt1, pt2
